02e605adf72e3d748e74efbfb37d9ff8410f199c
Chanel Mendoza <chanelmdza@gmail.com>
2022-12-03 20:42:22 -0800

    Add getPokemonList to pokemon api

--- app/src/main/java/com/chanel/android/pokedex/network/PokemonAPI.kt
+++ app/src/main/java/com/chanel/android/pokedex/network/PokemonAPI.kt
@@ -1,13 +1,21 @@
 package com.chanel.android.pokedex.network
 
 import com.chanel.android.pokedex.model.Pokemon
+import com.chanel.android.pokedex.model.PokemonListQueryResult
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface PokemonAPI {
 
     @GET("pokemon/{id}")
     suspend fun getPokemonInfo(@Path("id") id: String): Response<Pokemon>
+
+    @GET("pokemon")
+    suspend fun getPokemonList(
+        @Query("offset") offset: Int,
+        @Query("limit") limit: Int,
+    ): Response<PokemonListQueryResult>
 
 }
