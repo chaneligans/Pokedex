2912452bd6d554253e6d32d38cfb3b480f6f3b4d
Chanel Mendoza <chanelmdza@gmail.com>
2022-12-07 23:24:40 -0800

    Some styling

--- app/src/main/java/com/chanel/android/pokedex/PokemonItemHolder.kt
+++ app/src/main/java/com/chanel/android/pokedex/PokemonItemHolder.kt
@@ -3,12 +3,16 @@ package com.chanel.android.pokedex
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.setMargins
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.chanel.android.pokedex.databinding.PokemonItemBinding
 import com.chanel.android.pokedex.helpers.Helper.getPokemonImageUrl
+import com.chanel.android.pokedex.helpers.Helper.getTypeColor
 import com.chanel.android.pokedex.helpers.Helper.getValidId
 import com.chanel.android.pokedex.model.Pokemon
+import java.util.Locale
 
 class PokemonItemHolder(
     private val binding: PokemonItemBinding
@@ -19,22 +23,47 @@ class PokemonItemHolder(
         onPokemonClicked: (Pokemon) -> Unit
     ) {
         binding.apply {
+            // Clear Pokemon type layout
             pokemonInfoLayout.removeAllViews()
+
+            // Load Pokemon image in
             val validId = getValidId(pokemon.id)
             val imageUrl = getPokemonImageUrl(pokemon.id)
             pokemonImage.load(imageUrl)
+
+            // Set ID and name
             pokemonIdText.text = "#$validId"
-            pokemonNameText.text = pokemon.name
+            pokemonNameText.text = pokemon.name.replaceFirstChar {
+                if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
+            }
+
+            // Create textviews for each type and add to view
             pokemon.types.forEach { type ->
+                // Create text view and add margins/padding
                 val textView = TextView(pokemonNameText.context)
-                textView.layoutParams = LinearLayout.LayoutParams(
+                val layoutParams = LinearLayout.LayoutParams(
                     ViewGroup.LayoutParams.WRAP_CONTENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT
                 )
-                textView.text = type.typeResult.name
+                layoutParams.setMargins(5)
+                textView.layoutParams = layoutParams
+                textView.setPadding(20, 5, 20, 5)
+
+                // Set text with first letter capitalized
+                textView.text = type.typeResult.name.replaceFirstChar {
+                    if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
+                }
+
+                // Add style
+                textView.setTextAppearance(R.style.type_text)
+                val backgroundColor = ContextCompat.getColor(pokemonNameText.context, getTypeColor(type.typeResult.name))
+                textView.setBackgroundColor(backgroundColor)
+
+                // Add to layout
                 pokemonInfoLayout.addView(textView)
             }
 
+            // Add onClick
             binding.root.setOnClickListener {
                 onPokemonClicked(pokemon)
             }
--- app/src/main/java/com/chanel/android/pokedex/helpers/Helper.kt
+++ app/src/main/java/com/chanel/android/pokedex/helpers/Helper.kt
@@ -1,5 +1,7 @@
 package com.chanel.android.pokedex.helpers
 
+import com.chanel.android.pokedex.R
+
 object Helper {
 
     fun getPokemonImageUrl(id: Int): String {
@@ -15,5 +17,29 @@ object Helper {
             zeroes += "0"
         }
         return "$zeroes$idString"
+    }
+
+    fun getTypeColor(type: String): Int {
+        return when (type) {
+            "normal" -> R.color.normal
+            "fire" -> R.color.fire
+            "water" -> R.color.water
+            "electric" -> R.color.electric
+            "grass" -> R.color.grass
+            "ice" -> R.color.ice
+            "fighting" -> R.color.fighting
+            "poison" -> R.color.poison
+            "ground" -> R.color.ground
+            "flying" -> R.color.flying
+            "psychic" -> R.color.psychic
+            "bug" -> R.color.bug
+            "rock" -> R.color.rock
+            "ghost" -> R.color.ghost
+            "dragon" -> R.color.dragon
+            "dark" -> R.color.dark
+            "steel" -> R.color.steel
+            "fairy" -> R.color.fairy
+            else -> 0
+        }
     }
 }
